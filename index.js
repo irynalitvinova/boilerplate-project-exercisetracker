800eae46a0d4b1e023c26394387829b7ad02b737
Iryna Litvinova <lytvynlysko@gmail.com>
2023-09-10 20:58:26 +0300

    Add POST to /api/users

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URL, () => {
+  console.log("Connect to DB Succesefully");
+});
+
 app.use(cors())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
+// POST /api/users username
+app.post('/api/users', function (req, res) {
+  res.send(req.body.username);
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
