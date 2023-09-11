4789d9741849e9d9a553e1c920e1fabaa85eb7fa
Iryna Litvinova <lytvynlysko@gmail.com>
2023-09-11 19:34:35 +0300

    Add response from POST /api/users with form data username and this response is an object with username and _id properties.

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGO_URL, () => {
+//   console.log("Connect to DB Succesefully");
+// });
+
+
+
+// using localhost :
+/*
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:3000/posts", {
+  useNewUrlParser: true, useUnifiedTopology: true
+},
+  function () {
+    console.log("Connect to DB Succesefully");
+  }
+);
+*/
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URL, () => {
-  console.log("Connect to DB Succesefully");
-});
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true, useUnifiedTopology: true
+},
+  function () {
+    console.log("Connect to DB Succesefully");
+  }
+);
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+    },
+  }, 
+  { versionKey: false }
+);
+
+const User = mongoose.model('User', userSchema);
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
@@ -16,8 +51,17 @@ app.get('/', (req, res) => {
 });
 
 // POST /api/users username
-app.post('/api/users', function (req, res) {
-  res.send(req.body.username);
+app.post('/api/users', async (req, res) => {
+  //  res.send(req.body.username);
+  const username = req.body.username;
+  const foundUser = await User.findOne({ username });
+  if(foundUser) {
+    res.json(foundUser)
+  }
+  const user = await User.create({
+    username,
+  });
+ res.json(user);
 });
 
 
